5876037a471e9844d1575eab641b1b95db2477e6
anies1212 <all.for.me1212sna@gmail.com>
2022-08-04 22:04:34 +0900

    Initial Commit

---
+++ StoreKit_TutorialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreKit_TutorialApp
+//
+//  Created by 新垣 清奈 on 2022/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreKit_TutorialApp/StoreKit_TutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit_TutorialAppApp.swift
+//  StoreKit_TutorialApp
+//
+//  Created by 新垣 清奈 on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit_TutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
